91aa8ce5e3d716553b5a9d106483d9e1eafeb547
Abdel <113614023+TAHIRI1985@users.noreply.github.com>
2023-01-08 20:36:03 +0100

    ....

--- pages/admin/posts/add.php
+++ pages/admin/posts/add.php
@@ -24,7 +24,6 @@ if (!empty($_POST)) {
 
 
 
-
 $categories = App::getInstance()->getTable('Categorie')->extract('id', 'nom_Categorie');
 $form = new \Core\HTML\BootstrapForm($_POST);
 
@@ -32,40 +31,38 @@ $form = new \Core\HTML\BootstrapForm($_POST);
 
 
 
-<form method="post">
-    <h1>Ajouter l'article</h1>
 
-    <form method=post>
-        <fieldset>
+<h1>Ajouter l'article</h1>
 
-            <div class="mb-3">
-                <label class="form-label">Titre</label>
-                <input type="text" name="nom_Article" id="disabledTextInput" class="form-control">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Quantité</label>
-                <input type="number" name="Quantité" id="disabledTextInput" class="form-control">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Description</label>
-                <input type="text" name="description_Article" id="disabledTextInput" class="form-control">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Prix</label>
-                <input type="text" name="prix_Article" id="disabledTextInput" class="form-control">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Image1</label>
-                <input type="file" name="image1_Article" id="disabledTextInput" class="form-control" required>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Image2</label>
-                <input type="file" name="image2_Article" id="disabledTextInput" class="form-control" required>
-            </div>
-            <?= $form->select('id_categories', 'Catégorie ', $categories); ?>
-            <button type="submit" class="btn btn-primary">Publier</button>
+<form method=post>
+    <fieldset>
 
-        </fieldset>
-    </form>
+        <div class="mb-3">
+            <label class="form-label">Titre</label>
+            <input type="text" name="nom_Article" id="disabledTextInput" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Quantité</label>
+            <input type="number" name="Quantité" id="disabledTextInput" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Description</label>
+            <input type="text" name="description_Article" id="disabledTextInput" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Prix</label>
+            <input type="text" name="prix_Article" id="disabledTextInput" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Image1</label>
+            <input type="file" name="image1_Article" id="disabledTextInput" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Image2</label>
+            <input type="file" name="image2_Article" id="disabledTextInput" class="form-control" required>
+        </div>
+        <?= $form->select('id_categories', 'Catégorie ', $categories); ?>
+        <button type="submit" class="btn btn-primary">Publier</button>
 
+    </fieldset>
 </form>
